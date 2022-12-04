542712694db899d3cf11ab11a57ddd9beb1d3034
JakubKub13 <jakubkubala3@gmail.com>
2022-12-04 06:48:10 +0100

    Adding funcs in Yul

--- contracts/yulERC20.sol
+++ contracts/yulERC20.sol
@@ -14,6 +14,16 @@ object "Token" {
 
             // Dispatcher
             switch selector()
+            case 0x70a08231 /** balanceOf(address) */ {
+                returnUint(balanceOf(decodeAsAddress(0)))
+            }
+            case 0x18160ddd /** totalSupply() */ {
+                returnUint(totalSupply())
+            }
+            case 0xa9059cbb /** transfer(address, uint256) */ {
+                transfer(decodeAsAddress(0), decodeAsUnit(1))
+                returnTrue()
+            }
         }
     }
 }
